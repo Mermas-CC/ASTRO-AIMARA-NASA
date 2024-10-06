1027a5db2caf93b1bc0d69b9c9f8dc98a6954739
Mermas-CC <27junio03@gmail.com>
2024-10-06 00:57:19 -0500

    casi casi

--- src/components/MissionsSection.js
+++ src/components/MissionsSection.js
@@ -1,24 +1,31 @@
-// components/MissionsSection.js
+// src/components/MissionsSection.js
 import React from 'react';
-import planetData from '../data/planetas.json'; // Asegúrate de que la ruta sea correcta
 
-const MissionsSection = ({ onPlanetSelect }) => {
-    return (
-        <div className="p-4">
-            <h2 className="text-xl font-bold">Selecciona un Planeta</h2>
-            <ul className="space-y-2">
-                {planetData.map((planet) => (
-                    <li
-                        key={planet.pl_name}
-                        onClick={() => onPlanetSelect(planet)} // Llama a la función cuando se hace clic
-                        className="cursor-pointer hover:text-blue-500"
-                    >
-                        {planet.pl_name}
-                    </li>
-                ))}
-            </ul>
+const MissionsSection = () => {
+  return (
+    <section id="missions" className="py-20">
+      <div className="container mx-auto px-4">
+        <h2 className="text-4xl font-bold mb-8">Datos interesantes sobre Exoplanetas</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="bg-gray-800 p-6 rounded-lg">
+            <h3 className="text-2xl font-bold mb-4">Kepler-186f</h3>
+            <p>
+              Este es el primer exoplaneta del tamaño de la Tierra encontrado en la zona habitable de su estrella. 
+              Está a unos 500 años luz de distancia en la constelación de Cygnus.
+            </p>
+          </div>
+          <div className="bg-gray-800 p-6 rounded-lg">
+            <h3 className="text-2xl font-bold mb-4">TRAPPIST-1</h3>
+            <p>
+              Este sistema tiene siete exoplanetas del tamaño de la Tierra, tres de los cuales están en la zona habitable 
+              de la estrella enana ultrafría TRAPPIST-1, a 40 años luz de distancia.
+            </p>
+          </div>
+          
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default MissionsSection;
